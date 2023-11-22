a01c63d759ee82e45ea926871645f88b828b89cc
Tasharany <124934592+Tasharany@users.noreply.github.com>
2023-11-21 19:43:14 -0600

    did fluuter clean

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_windows_3.13.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Thuan\Documents\GitHub\kepu"
+export "FLUTTER_ROOT=C:\Users\Riacha\Downloads\flutter_windows_3.13.8-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Riacha\OneDrive - Louisiana State University\Documents\GitHub\kepu"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
